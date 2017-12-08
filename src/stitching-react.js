2c6135c25bc43dca70eaa5a97b680dc76ca277aa
WaterKhair <waterkahir@gmail.com>
2017-12-07 23:37:57 -0800

    Updating documentation and exporting stitching.

--- src/stitching-react.js
+++ src/stitching-react.js
@@ -3,17 +3,18 @@ import ConfirmComponent from "./components/confirm";
 import LoginComponent from "./components/login";
 import ProfileComponent from "./components/profile";
 import ResetPasswordComponent from "./components/reset_password";
+import Stitching from "stitching";
 import {THEME_COLORS} from "./common";
 import TableComponent from "./components/table";
-import stitching from "stitching";
 
-export const {connect} = stitching;
 export const Confirm = ConfirmComponent;
 export const Login = LoginComponent;
 export const Profile = ProfileComponent;
 export const ResetPassword = ResetPasswordComponent;
 export const Table = TableComponent;
 export const ThemeColors = THEME_COLORS;
+export const {connect} = Stitching;
+export const stitching = Stitching;
 
 export default {
     Confirm,
@@ -22,6 +23,7 @@ export default {
     ResetPassword,
     Table,
     ThemeColors,
-    connect
+    connect,
+    stitching
 };
 
